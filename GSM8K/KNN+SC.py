86f3bf7d51eaf9962d356954e92222a2eb1e95af
Kiran Purohit <kiran.purohit789@gmail.com>
2024-03-03 21:26:04 +0530

    Update and rename MMR+SC.py to KNN+SC.py

--- GSM8K/KNN+SC.py
+++ GSM8K/KNN+SC.py
@@ -76,7 +76,7 @@ def compare_llm_outputs(user_query):
 
     return results
 
-def mmr(doc_embeddings, query_embedding, lambda_param, top_k):
+def knn(doc_embeddings, query_embedding, lambda_param, top_k):
     # Normalize embeddings
     doc_embeddings = doc_embeddings / np.linalg.norm(doc_embeddings, axis=1, keepdims=True)
     query_embedding = query_embedding / np.linalg.norm(query_embedding)
@@ -101,7 +101,7 @@ def mmr(doc_embeddings, query_embedding, lambda_param, top_k):
     max_relevance_index = np.argmax(relevance_scores)
     selected_set.add(max_relevance_index)
 
-    # Compute MMR scores and select documents iteratively
+    # Compute KNN scores and select documents iteratively
     while len(selected_set) < top_k:
         remaining_indices = list(set(similarity_indices[0]) - selected_set)
         remaining_embeddings = doc_embeddings[remaining_indices]
@@ -109,12 +109,12 @@ def mmr(doc_embeddings, query_embedding, lambda_param, top_k):
         # Compute similarity with the query for remaining documents
         similarity_scores = np.dot(remaining_embeddings, query_embedding)
         # print(similarity_scores)
-        # Compute MMR scores
-        mmr_scores = lambda_param * relevance_scores[remaining_indices] - (1 - lambda_param) * similarity_scores
+        # Compute KNN scores
+        knn_scores = lambda_param * relevance_scores[remaining_indices] - (1 - lambda_param) * similarity_scores
 
-        # Select document with maximum MMR score
-        max_mmr_index = remaining_indices[np.argmax(mmr_scores)]
-        selected_set.add(max_mmr_index)
+        # Select document with maximum KNN score
+        max_knn_index = remaining_indices[np.argmax(knn_scores)]
+        selected_set.add(max_knn_index)
 
     # Convert selected set to a list of indices
     selected_indices = list(selected_set)
@@ -196,7 +196,7 @@ if __name__ == "__main__":
 
     # Set the number of documents to return
     top_k = 5
-    # Apply MMR function
+    # Apply KNN function
     exnum = 0
     matches = 0
     mismatches = 0
@@ -206,7 +206,7 @@ if __name__ == "__main__":
 
         user_query = "Follow given examples and solve the Test Question at end in similar manner by giving step by step reasoning followed by the Final Answer.\n\n"
 
-        selected_indices = mmr(train_emb, test_emb[exnum], lambda_param, top_k)
+        selected_indices = knn(train_emb, test_emb[exnum], lambda_param, top_k)
 
         print("\nSelected Indices:", selected_indices)
 
