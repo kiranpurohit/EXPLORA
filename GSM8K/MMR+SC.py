2a74ddacc7c8340bbc3b69c941638033ff78b287
Raghuram510 <56595960+Raghuram510@users.noreply.github.com>
2024-03-06 19:13:05 +0530

    Update MMR+SC.py

--- GSM8K/MMR+SC.py
+++ GSM8K/MMR+SC.py
@@ -158,8 +158,8 @@ def self_con(tmp_list):
     ans_list = []
     for tmp in tmp_list:
         ans = ""
-        if len(tmp.split("Final Answer:"))>1:
-            ans = tmp.split("Final Answer:")[1]
+        if len(tmp.split("Final Answer:"))>6:
+            ans = tmp.split("Final Answer:")[6]
             ans = ans.split("\n")[0]
             # print(ans)
             if "each" in ans:  ans = ans.split("each")[0]
